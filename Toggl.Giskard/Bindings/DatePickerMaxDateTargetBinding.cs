49d4f4db89476c58ad0b9282f9de016bd1ebdeea
kornelijepetak <kornelije@gmail.com>
2018-05-15 06:11:03 +0200

    :bug: Fix min and max bindings

--- Toggl.Giskard/Bindings/DatePickerMaxDateTargetBinding.cs
+++ Toggl.Giskard/Bindings/DatePickerMaxDateTargetBinding.cs
@@ -23,7 +23,17 @@ namespace Toggl.Giskard.Bindings
 
         protected override void SetValueImpl(DatePicker target, DateTimeOffset value)
         {
-            target.MaxDate = value.ToUnixTimeMilliseconds();
+            var utcValue = value.ToLocalTime();
+            var upperBoundary = (DateTimeOffset)utcValue.Date.AddDays(1);
+            var nextYear = utcValue.AddYears(1);
+
+            // Workaround for a DatePicker bug in which
+            // there's an early return if the year is the same
+            // and the dates are different, which is bad logic.
+            // https://stackoverflow.com/a/19722636/93770
+            target.MaxDate = nextYear.ToUnixTimeMilliseconds();
+
+            target.MaxDate = upperBoundary.ToUnixTimeMilliseconds();
         }
     }
 }
--- Toggl.Giskard/Bindings/DatePickerMinDateTargetBinding.cs
+++ Toggl.Giskard/Bindings/DatePickerMinDateTargetBinding.cs
@@ -23,7 +23,16 @@ namespace Toggl.Giskard.Bindings
 
         protected override void SetValueImpl(DatePicker target, DateTimeOffset value)
         {
-            target.MinDate = value.ToUnixTimeMilliseconds();
+            var utcValue = value.ToUniversalTime();
+            var lowerBoundary = (DateTimeOffset)utcValue.Date;
+
+            // Workaround for a DatePicker bug in which
+            // there's an early return if the year is the same
+            // and the dates are different, which is bad logic.
+            // https://stackoverflow.com/a/19722636/93770
+            target.MinDate = 0;
+
+            target.MinDate = lowerBoundary.ToUnixTimeMilliseconds();
         }
     }
 }
