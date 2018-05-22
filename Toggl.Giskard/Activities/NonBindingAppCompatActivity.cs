ecf6f178b18bca89c9a8cc3bf204477c870fbd47
William Barbosa <william.sb@hotmail.com>
2018-05-22 16:55:06 -0300

    :robot: Add activity to avoid binding inflate

---
+++ Toggl.Giskard/Activities/NonBindingAppCompatActivity.cs
@@ -0,0 +1,54 @@
+﻿using Android.OS;
+using MvvmCross.Core.ViewModels;
+using MvvmCross.Droid.Support.V7.AppCompat.EventSource;
+
+namespace Toggl.Giskard.Activities
+{
+    public class NonBindingAppCompatActivity<TViewModel> : MvxEventSourceAppCompatActivity
+        where TViewModel : class, IMvxViewModel 
+    {
+        public object DataContext { get; set; }
+
+        public TViewModel ViewModel
+        {
+            get => DataContext as TViewModel;
+            set => DataContext = value;
+        }
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            ViewModel?.ViewCreated();
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            ViewModel?.ViewDestroy();
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+            ViewModel?.ViewAppearing();
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            ViewModel?.ViewAppeared();
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+            ViewModel?.ViewDisappearing();
+        }
+
+        protected override void OnStop()
+        {
+            base.OnStop();
+            ViewModel?.ViewDisappeared();
+        }
+    }
+}
