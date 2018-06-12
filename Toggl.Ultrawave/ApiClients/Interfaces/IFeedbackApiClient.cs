c58b00008656dd45b32aecce384201754a678ca7
Simon Rozsival <simon@rozsival.com>
2018-06-12 10:14:07 +0200

    :sparkles: Add API client

---
+++ Toggl.Ultrawave/ApiClients/Interfaces/IFeedbackApiClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive;
+using Toggl.Multivac;
+
+namespace Toggl.Ultrawave.ApiClients
+{
+    public interface IFeedbackApiClient
+    {
+        IObservable<Unit> Send(Email email, string message, IDictionary<string, string> data);
+    }
+}
