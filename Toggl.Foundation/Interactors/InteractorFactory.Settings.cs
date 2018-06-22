0fc2e01691d69f264989bae859122211c5d3bb34
Simon Rozsival <simon@rozsival.com>
2018-06-22 13:24:19 +0200

    :construction: Add interactor factory

---
+++ Toggl.Foundation/Interactors/InteractorFactory.Settings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reactive;
+using Toggl.Foundation.Interactors.Settings;
+
+namespace Toggl.Foundation.Interactors
+{
+    public partial class InteractorFactory : IInteractorFactory
+    {
+        public IInteractor<IObservable<Unit>> SendFeedback(string message)
+            => new SendFeedbackInteractor(
+                null, // todo: Inject the API as soon as its PR is merged.
+                dataSource.User,
+                dataSource.Workspaces,
+                dataSource.TimeEntries,
+                platformConstants,
+                userPreferences,
+                lastTimeUsageStorage,
+                timeService,
+                userAgent,
+                message);
+    }
+}
